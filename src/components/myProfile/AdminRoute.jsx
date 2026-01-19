7957bee6faa35f9048f6d3074b329213480582d2
Shihab Shahriar <2022100010087@seu.edu.bd>
2026-01-20 01:01:02 +0600

    added page and chnage padding

--- src/components/myProfile/AdminRoute.jsx
+++ src/components/myProfile/AdminRoute.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { Navigate } from "react-router";
+import useAuth from "../useAuth";
+import { useEffect, useState } from "react";
 
-const AdminRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AdminRoute = ({ children }) => {
+  const { user } = useAuth();
+  const [isAdmin, setIsAdmin] = useState(null);
+
+  useEffect(() => {
+    if (user?.email) {
+      fetch(`http://localhost:5000/users/admin/${user.email}`)
+        .then(res => res.json())
+        .then(data => setIsAdmin(data.admin));
+    }
+  }, [user]);
+
+  if (isAdmin === null) return <p>Loading...</p>;
+
+  return isAdmin ? children : <Navigate to="/" />;
 };
 
 export default AdminRoute;
